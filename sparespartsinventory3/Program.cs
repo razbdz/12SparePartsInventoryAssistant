0d2b8f1039bb397ac3c92dc130f0b440127a0d1e
Razije <s236154@dtu.dk>
2025-11-23 21:34:53 +0100

    den nye

---
+++ sparespartsinventory3/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    { string[] inventory = { "hydraulic pump", "PLC module", "servo motor" };
+
+        Console.WriteLine("Hej. Welcome to the spare parts inventory!");
+
+        while (true)
+        { Console.Write("Which part do you need? ");
+            string userInput = Console.ReadLine() ?? "";
+
+         
+            if (userInput == "Do you actually have any parts?" ||
+                userInput == "Do you actualy have any parts?" || 
+                userInput == "Is there anything in stock at all?")
+            { Console.WriteLine($"We have {inventory.Length} part(s)!");
+                foreach (string item in inventory)
+                {
+                    Console.WriteLine(item);
+                }
+                continue;
+            }
+            bool match = Array.Exists(inventory, part => part == userInput);
+
+            if (match)
+            { Console.WriteLine($"I've got {userInput} here for you");
+                break; 
+            }
+            else
+            { Console.WriteLine($"I am afraid we don't have any {userInput} in the inventory");
+            }
+        }
+    }
+}
